5540d9160421abc22530d71412eaf46dbc842a15
TinsFox <fox@tinsfox.com>
2023-07-08 22:08:12 +0800

    feat: use hook form

--- src/pages/user/setting.tsx
+++ src/pages/user/setting.tsx
@@ -1,7 +1,6 @@
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Ear, Mail, Mic, PlusSquare, Volume1, Waves } from 'lucide-react'
-import { useState } from 'react'
 import {
   Select,
   SelectContent,
@@ -23,27 +22,28 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  email: z.string().min(2).max(50),
+  subscription: z.string().min(2).max(50),
+  language: z.string().min(2).max(50),
+  speed: z.string().min(2).max(50),
 })
 export default function Setting() {
-  const [email, setEmail] = useState('ai@convo.com')
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value)
-  }
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: '',
+      email: 'ai@convo.com',
+      subscription: 'Free',
+      language: 'English',
+      speed: '1.25',
     },
   })
 
@@ -63,59 +63,85 @@ export default function Setting() {
         <form className="mt-12" onSubmit={form.handleSubmit(onSubmit)}>
           <div>
             <Label className="ml-[18px] text-slate-500">ACCOUNT</Label>
-            <div className="flex items-center mt-2 bg-white">
-              <div className="flex items-center flex-1 w-1/3 h-12 ml-4">
-                <Mail width={16} height={16}></Mail>
-                <Label className="ml-2">Email</Label>
-              </div>
-              <Input
-                className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={handleEmailChange}
-              />
-            </div>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="flex items-center mt-2 bg-white">
+                  <FormLabel className="flex items-center flex-1 w-1/3 h-12 ml-4">
+                    <Mail width={16} height={16}></Mail>
+                    <Label className="ml-2">Email</Label>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
+                      type="email"
+                      placeholder="Enter your email"
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
             <Separator></Separator>
-            <div className="flex items-center bg-white ">
-              <div className="flex items-center flex-1 w-1/3 h-12 ml-4">
-                <PlusSquare width={16} height={16}></PlusSquare>
-                <Label className="ml-2">Subscription</Label>
-              </div>
-              <Input
-                className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
-                disabled
-                placeholder="Free"
-                value={email}
-                onChange={handleEmailChange}
-              />
-            </div>
+
+            <FormField
+              control={form.control}
+              name="subscription"
+              render={({ field }) => (
+                <FormItem className="flex items-center bg-white">
+                  <FormLabel className="flex items-center flex-1 w-1/3 h-12 ml-4">
+                    <PlusSquare width={16} height={16}></PlusSquare>
+                    <Label className="ml-2">Subscription</Label>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
+                      disabled
+                      placeholder="Free"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
 
           <div className="mt-8">
             <Label className="ml-[18px] text-slate-500">SPEECH</Label>
-            <div className="flex items-center mt-2 bg-white border-b">
-              <div className="flex items-center flex-1 w-1/3 h-12 ml-4">
-                <Mic width={16} height={16}></Mic>
-                <Label className="ml-2">Language</Label>
-              </div>
-              <Select>
-                <SelectTrigger className="w-32 ml-2 border-none">
-                  <SelectValue placeholder="Auto-Detect" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="apple">Auto-Detect</SelectItem>
-                  <SelectItem value="banana">Banana</SelectItem>
-                  <SelectItem value="blueberry">Blueberry</SelectItem>
-                  <SelectItem value="grapes">Grapes</SelectItem>
-                  <SelectItem value="pineapple">Pineapple</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
+
+            <FormField
+              control={form.control}
+              name="language"
+              render={({ field }) => (
+                <FormItem className="flex items-center bg-white">
+                  <FormLabel className="flex items-center flex-1 w-1/3 h-12 ml-4">
+                    <Mic width={16} height={16}></Mic>
+                    <Label className="ml-2">Language</Label>
+                  </FormLabel>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger className="w-32 ml-2 border-none">
+                        <SelectValue placeholder="Select a verified email to display" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="English">Auto-Detect</SelectItem>
+                      <SelectItem value="zh-CN">zh-CN</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
 
           <div className="mt-8">
             <Label className="ml-[18px] text-slate-500">VOICE</Label>
+
             <Accordion type="multiple" className="w-full mt-2 bg-white">
               <AccordionItem value="item-1">
                 <AccordionTrigger className="mr-[18px]">
@@ -125,10 +151,100 @@ export default function Setting() {
                   </div>
                   <Input
                     className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
+                    disabled
+                    value={form.watch('speed')}
+                  />
+                </AccordionTrigger>
+                <AccordionContent>
+                  <FormField
+                    control={form.control}
+                    name="speed"
+                    render={({ field }) => (
+                      <FormItem className="space-y-0">
+                        <FormControl className="p-0">
+                          <RadioGroup
+                            className="grid grid-cols-7 gap-0 px-2 mx-8 my-2 border-2 rounded-md"
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <FormItem className="flex items-center space-y-0">
+                              <FormControl>
+                                <Label className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm">
+                                  <RadioGroupItem value="0.5" className="sr-only" />
+                                  0.5
+                                </Label>
+                              </FormControl>
+                            </FormItem>
+
+                            <FormItem className="flex items-center space-y-0">
+                              <FormControl>
+                                <Label className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm">
+                                  <RadioGroupItem value="0.75" className="sr-only" />
+                                  0.75
+                                </Label>
+                              </FormControl>
+                            </FormItem>
+                            <FormItem className="flex items-center space-y-0">
+                              <FormControl>
+                                <Label className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm">
+                                  <RadioGroupItem value="1" className="sr-only" />1
+                                </Label>
+                              </FormControl>
+                            </FormItem>
+
+                            <FormItem className="flex items-center space-y-0">
+                              <FormControl>
+                                <Label className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm">
+                                  <RadioGroupItem value="1.25" />
+                                  1.25
+                                </Label>
+                              </FormControl>
+                            </FormItem>
+
+                            <FormItem className="flex items-center space-y-0">
+                              <FormControl>
+                                <Label className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm">
+                                  <RadioGroupItem value="1.5" className="sr-only" />
+                                  1.5
+                                </Label>
+                              </FormControl>
+                            </FormItem>
+
+                            <FormItem className="flex items-center space-x-2 space-y-0">
+                              <FormControl>
+                                <Label className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm">
+                                  <RadioGroupItem value="1.75" className="sr-only" />
+                                  1.75
+                                </Label>
+                              </FormControl>
+                            </FormItem>
+
+                            <FormItem className="flex items-center space-x-2 space-y-0">
+                              <FormControl>
+                                <Label className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm">
+                                  <RadioGroupItem value="2" className="sr-only" />2
+                                </Label>
+                              </FormControl>
+                            </FormItem>
+                          </RadioGroup>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </AccordionContent>
+              </AccordionItem>
+
+              {/* <AccordionItem value="item-1">
+                <AccordionTrigger className="mr-[18px]">
+                  <div className="flex items-center flex-1 w-1/3 h-12 ml-4">
+                    <Waves width={16} height={16}></Waves>
+                    <Label className="ml-2">Speed</Label>
+                  </div>
+                  <Input
+                    className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
                     type="email"
                     placeholder="Enter your email"
-                    value={email}
-                    onChange={handleEmailChange}
                   />
                 </AccordionTrigger>
                 <AccordionContent>
@@ -137,17 +253,17 @@ export default function Setting() {
                     className="grid grid-cols-6 gap-0 px-2 mx-8 my-2 border-2 rounded-md"
                   >
                     <Label
-                      htmlFor="paypal"
+
                       className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm"
                     >
-                      <RadioGroupItem value="paypal" id="paypal" className="sr-only" />
+                      <RadioGroupItem value="paypal"  className="sr-only" />
                       0.5
                     </Label>
                     <Label
-                      htmlFor="paypal"
+
                       className="border-muted bg-popover p-2 hover:bg-accent hover:text-accent-foreground [&:has([data-state=checked])]:border-primary text-slate-900 font-medium text-sm"
                     >
-                      <RadioGroupItem value="paypal" id="paypal" className="sr-only" />
+                      <RadioGroupItem value="paypal"  className="sr-only" />
                       0.75
                     </Label>
                     <Label
@@ -180,7 +296,7 @@ export default function Setting() {
                     </Label>
                   </RadioGroup>
                 </AccordionContent>
-              </AccordionItem>
+              </AccordionItem> */}
 
               <AccordionItem value="item-12">
                 <AccordionTrigger className="mr-[18px]">
@@ -192,8 +308,6 @@ export default function Setting() {
                     className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
                     disabled
                     placeholder="Enter your email"
-                    value={email}
-                    onChange={handleEmailChange}
                   />
                 </AccordionTrigger>
                 <AccordionContent>
@@ -211,8 +325,6 @@ export default function Setting() {
                     className="w-2/3 ml-2 text-right border-none focus-visible:outline-none focus-visible:ring-0"
                     disabled
                     placeholder="Enter your email"
-                    value={email}
-                    onChange={handleEmailChange}
                   />
                 </AccordionTrigger>
                 <AccordionContent>
